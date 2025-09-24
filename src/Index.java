fa02a69ea52c6408cb84eae22d67b3265b77e28d
Pooja Patel <studykhuswaha01@gmail.com>
2025-09-24 21:56:37 +0530

    Add index class

---
+++ src/Index.java
@@ -0,0 +1,17 @@
+public class Index
+{
+    public static void main(String[] args)
+    {
+    /*     int number1,number2,result;
+           number1 =14;
+           number2 =2;
+           result= number1 + number2;
+           System.out.println("sum of two number is:" + result);  */
+
+
+        int number =12;
+        System.out.println("number is:" + number);
+        System.out.println("number is:" + number);
+        System.out.println("number is:" + number);
+    }
+}
